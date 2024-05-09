54a5ec952f917180c57caa13d1a6367bcc02f229
deepakdevcast <dk8232847@gmail.com>
2024-05-09 22:25:07 +0530

    frontend boilerplate; react, tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className='flex h-screen items-center justify-center text-xl font-bold'>Fintech Frontend</div>
+    </>
+  )
+}
+
+export default App
