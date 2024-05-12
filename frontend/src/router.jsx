aab18f2cb5abf29db7b77672c7283c94d5c402b5
deepakdevcast <dk8232847@gmail.com>
2024-05-12 13:20:04 +0530

    routers

---
+++ frontend/src/router.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable react/prop-types */
+import {  Link, RouterProvider, createBrowserRouter } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import SendMoney from "./pages/SendMoney";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/signup",
+    element: <SignUp />
+  },
+  {
+    path: "/signin",
+    element: <SignIn />
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+    errorElement: <div className="h-screen flex flex-col justify-center items-center">
+      <h1 className="text-2xl">Unauthorized! Please SignIn/SignUp!</h1>
+      <div>
+        <Link to={'/signin'} className="px-4 py-2 rounded-md text-blue-500">Sign In</Link>{' '}
+        <Link to={'/signup'} className="px-4 py-2 rounded-md text-blue-500">Sign Up</Link>
+      </div>
+    </div>
+  },
+  {
+    path: "/transfer",
+    element: <SendMoney />,
+    errorElement: <div className="h-screen flex flex-col justify-center items-center">
+      <h1 className="text-2xl">Unauthorized! Please SignIn/SignUp!</h1>
+      <div>
+        <Link to={'/signin'} className="px-4 py-2 rounded-md text-blue-500">Sign In</Link>{' '}
+        <Link to={'/signup'} className="px-4 py-2 rounded-md text-blue-500">Sign Up</Link>
+      </div>
+    </div>
+  }
+])
+function InitRouter() {
+  return (
+    <RouterProvider router={router}/>
+  )
+}
+
+export default InitRouter;
